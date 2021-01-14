5c4644f2ddbdce3c1f59f0f0f9994b086cffb7df
Diego Pagini <diegopaginidev@gmail.com>
2021-01-14 15:00:07 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { translations, translationChunksConfig } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl: 'https://localhost:9002',
+          prefix: '/rest/v2/'
+        }
+      },
+      context: {
+        currency: ['USD'],
+        language: ['en'],
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en'
+      },
+      features: {
+        level: '2.1'
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
